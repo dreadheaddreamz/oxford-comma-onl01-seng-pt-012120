c8db056643eb94e791652bf85f57a05ea2a980ce
James Batsell <jamesbatsell05@gmail.com>
2020-02-01 19:44:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
  if array.length == 2 
    return "#{array[0]} and #{array[1]}"
-   elsif
-   array 2 < array.length
+   elsif array 2 < array.length
    array[-1].insert(0, "and")
  end
  array.join(",")
